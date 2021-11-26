80d8d4862aeaed07b283500563321548ff6fc663
Adam Tahri <gotakk@gmail.com>
2021-11-26 12:17:12 +0100

    update to 1.2.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64         = 'https://github.com/iotaledger/firefly/releases/download/desktop-1.1.0/firefly-desktop-1.1.0.exe'
+$url64         = 'https://github.com/iotaledger/firefly/releases/download/desktop-1.2.0/firefly-desktop-1.2.0.exe'
 
 $GoShimmerPackageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $GoShimmerPackageArgs = @{
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'firefly*'
-  checksum64    = '755096cb253569c9d61688f93fafe7350c3349e2d48707a93a5da986c286464a'
+  checksum64    = 'b765bb343caac52d1c6de71a30f29887b2debaf298cfe169893e1cb0eccb98ac'
   checksumType64= 'sha256'
   validExitCodes= @(0, 3010, 1641)
 }
