7f0f2fe48cb145e8caa7711cca2cca9c4f9318cb
Adam Tahri <gotakk@gmail.com>
2022-01-24 20:20:22 +0100

    update to 1.3.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url64         = 
+$url64         = 'https://dl.firefly.iota.org/firefly-desktop-1.3.3.exe'
+$checksum64    = 'bc9a13b0b5f3c3e3bcd598a62c2c1b3575a8bdde3f41709ade6ca24c829dcde5'
 
 $FireflyPackageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   silentArgs    = '/S --force-run'
-  url64bit      = 'https://dl.firefly.iota.org/firefly-desktop-1.3.2.exe'
+  url64bit      = $url64
   softwareName  = 'firefly*'
-  checksum64    = '9ef4be6d365f1c1e3164136af2c98fc654e4895f1b0efc33d9b892002b47630b'
+  checksum64    = checksum64
   checksumType64= 'sha256'
   validExitCodes= @(0, 3010, 1641)
 }
